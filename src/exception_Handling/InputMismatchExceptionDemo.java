1bf0a975fee6b08739a5bc99fc65473d6697d8a2
Rima Akter <rimawahid3@gmail.com>
2021-10-27 11:34:33 +0600

    mis match exception

---
+++ src/exception_Handling/InputMismatchExceptionDemo.java
@@ -0,0 +1,28 @@
+package exception_Handling;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class InputMismatchExceptionDemo {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean continueInput = true;
+        do {
+            try {
+                System.out.print("Enter an integer : ");
+                int num = input.nextInt();
+                
+                //Display the result
+                System.out.println("The number entered is " + num);
+                
+                continueInput = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Try again. (" +
+                        "Incorrect input : an integer is required)");
+                input.nextLine();
+            }
+
+        } while (continueInput);
+    }
+}
