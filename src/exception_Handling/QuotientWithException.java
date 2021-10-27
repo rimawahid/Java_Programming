9e3654ded1d4a0365cd6c3f93c1548e92b84f8d9
Rima Akter <rimawahid3@gmail.com>
2021-10-27 10:35:50 +0600

    throw exception

--- src/exception_Handling/QuotientWithException.java
+++ src/exception_Handling/QuotientWithException.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exception_Handling;
 
-/**
- *
- * @author USER
- */
+import java.util.Scanner;
+
 public class QuotientWithException {
-    
+
+    public static int quotient(int num1, int num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Divisor cannot be zero");
+        }
+        return num1 / num2;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter two integers : ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        try{
+            int result = quotient(num1,num2);
+            System.out.println(num1 + "/" + num2 + " is "+ result);
+        }catch(ArithmeticException e){
+            //System.out.println("Exception : an integer "
+             //      + "can not be divided by zero ");
+             System.out.println(e);
+        }
+        System.out.println("Execution cintunues...");
+    }
 }
