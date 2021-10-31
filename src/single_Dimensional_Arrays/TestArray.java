8ab414a8285eeebe019a9ec746f144ace792069e
Rima Akter <rimawahid3@gmail.com>
2021-10-31 11:47:29 +0600

    var args

--- src/single_Dimensional_Arrays/TestArray.java
+++ src/single_Dimensional_Arrays/TestArray.java
@@ -3,16 +3,24 @@ package single_Dimensional_Arrays;
 public class TestArray {
 
     public static void main(String[] args) {
-        int x = 1;
-        int[] y = new int[10];
+//        int x = 1;
+//        int[] y = new int[10];
+//
+//        m(x, y);
+//        System.out.println("x is : " + x);
+//        System.out.println("y is : " + y[0]);
 
-        m(x, y);
-        System.out.println("x is : " + x);
-        System.out.println("y is : " + y[0]);
+        int[] list = {1, 2, 3, 5, 4};
+        for (int i = 0, j = list.length-1; i < list.length/2; i++, j--) {
+            int temp = list[i];
+            list[i]= list[j];
+            list [j]= temp;
+            
+        }
     }
 
-    public static void m(int num, int[] numbers) {
-        num = 2002;
-        numbers[0] = 5555;
-    }
+//    public static void m(int num, int[] numbers) {
+//        num = 2002;
+//        numbers[0] = 5555;
+//    }
 }
